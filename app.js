a38b27eae56a4c66045a727dc300c7974e8ec846
liqiang <1281991712@qq.com>
2020-05-21 09:22:13 +0800

    1

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//允许跨域
+app.use(function(req, res, next) {
+  res.append("Access-Control-Allow-Origin", "*")
+  next()
+})
 //路由配置
 app.use("/api",router)
 
